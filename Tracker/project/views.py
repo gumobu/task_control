8023ddb555dccf728cebd706a02339044f1c433e
gumobu <kirill.fursaev@gmail.com>
2023-05-24 01:58:56 +0300

    Добавлено новое приложение для хранения логики задач

--- Tracker/project/views.py
+++ Tracker/project/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     """Отображение всех проектов"""
-    return HttpResponse("Hello, world. Это индекс проекта")
+    return HttpResponse("Hello, world. Здесь будут находиться все проекты")
 
 
 def detail(request, project_id):
     """Отображение подробной информации о проекте"""
-    response = "Проект %s"
-    return HttpResponse(response % project_id)
+    response = "Подробная информация о проекте "
+    return HttpResponse(response + str(project_id))
