ec37be9ee4c72d785f209dba08158ad940b7559c
gumobu <kirill.fursaev@gmail.com>
2023-05-24 21:45:46 +0300

    Удалено приложение. "Задача" перенесена в "Проект"

--- Tracker/Tracker/urls.py
+++ Tracker/Tracker/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project/', include('project.urls')),
-    path('issue/', include('issue.urls')),
+    path('issue/', include('project.urls'))
 ]
--- Tracker/project/models.py
+++ Tracker/project/models.py
@@ -5,11 +5,11 @@ from django.utils import timezone
 
 class Project(models.Model):
     """Модель проекта с основными полями"""
-    id = models.CharField(max_length=15, unique=True, primary_key=True)  # Идентификатор проекта
-    title = models.CharField(max_length=200, unique=True)  # Название проекта
-    created_date = models.DateTimeField(auto_now_add=True)  # Дата создания проекта
-    start_date = models.DateTimeField(auto_now_add=True)  # Дата начала проекта
-    end_date = models.DateTimeField(blank=True, null=True)  # Дата окончания проекта
+    id = models.CharField(max_length=15, unique=True, primary_key=True, verbose_name='Проект')  # Идентификатор проекта
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название')  # Название проекта
+    created_date = models.DateTimeField(default=timezone.now(), verbose_name='Дата создания')  # Дата создания проекта
+    updated_date = models.DateTimeField(default=timezone.now(), verbose_name='Дата обновления')  # Дата обновления проектам
+    end_date = models.DateTimeField(null=True, verbose_name='Дата закрытия')  # Дата завершения проекта
 
     def end(self):
         """Метод для завершения проекта"""
@@ -26,4 +26,31 @@ class Project(models.Model):
 
     def __str__(self):
         """Строковое представление проекта - его идентификатор"""
+        return self.id
+
+
+class Issue(models.Model):
+    """Модель задачи с основными полями"""
+    id = models.CharField(max_length=15, unique=True, primary_key=True, verbose_name='Задача')  # Идентификатор задачи
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, verbose_name='Проект')
+    title = models.CharField(max_length=200, unique=True, verbose_name='Название')  # Название задачи
+    created_date = models.DateTimeField(default=timezone.now(), verbose_name='Дата создания')  # Дата создания задачи
+    updated_date = models.DateTimeField(default=timezone.now(), verbose_name='Дата обновления')  # Дата обновления задачи
+    end_date = models.DateTimeField(null=True, verbose_name='Дата закрытия')  # Дата завершения задачи
+
+    def end(self):
+        """Метод для завершения задачи"""
+        self.end_date = timezone.now()
+        self.save()
+
+    def create(self):
+        """Метод для создания задачи"""
+        pass
+
+    def update(self):
+        """Метод обновления задачи"""
+        pass
+
+    def __str__(self):
+        """Строковое представление задачи - ее идентификатор"""
         return self.id
