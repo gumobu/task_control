3fe19e597dac8fe63a11c5718f5e929ff3fa56ec
gumobu <kirill.fursaev@gmail.com>
2023-05-27 02:29:40 +0300

    Доработаны модели

--- Tracker/account/models.py
+++ Tracker/account/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class Division(models.Model):
     """Модель подразделения"""
     name = models.CharField(max_length=100, null=False, unique=True)
-
+    parent_division = models.ForeignKey('self', on_delete=models.SET_NULL)
 
 class Position(models.Model):
     """Модель должности сотрудника"""
@@ -15,8 +15,8 @@ class Position(models.Model):
 class Profile(models.Model):
     """Расширенная модель пользователя для хранения требуемой информации"""
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    division = models.ForeignKey(Division, on_delete=models.SET_NULL, null=True)
-    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
+    division = models.ForeignKey(Division, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
 
     def __str__(self):
         """Строковое представление пользователя"""
